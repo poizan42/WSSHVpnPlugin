5c050f8c0cbb46065ed7b90112520969423dec75
Kasper Fabæch Brandt <poizan@poizan.dk>
2026-08-14 05:22:43 +0200

    Initial commit

---
+++ PoiTech.WSSHVpnPlugin.App/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace PoiTech.WSSHVpnPlugin.App;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+/// </summary>
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PoiTech.WSSHVpnPlugin.VpnPlugin/SSHVpnPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Networking.Vpn;
+
+namespace PoiTech.WSSHVpnPlugin.VpnPlugin;
+
+public sealed class SSHVpnPlugin : IVpnPlugIn
+{
+    public void Connect(VpnChannel channel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Decapsulate(VpnChannel channel, VpnPacketBuffer encapBuffer, VpnPacketBufferList decapsulatedPackets, VpnPacketBufferList controlPacketsToSend)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Disconnect(VpnChannel channel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Encapsulate(VpnChannel channel, VpnPacketBufferList packets, VpnPacketBufferList encapulatedPackets)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetKeepAlivePayload(VpnChannel channel, out VpnPacketBuffer keepAlivePacket)
+    {
+        throw new NotImplementedException();
+    }
+}
